c373591bc461412d45343a6bac1bccda45433ec8
yunfeifan <yunfan777@gmail.com>
2022-07-16 16:01:37 +0800

    remove test

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,37 +1,20 @@
-import Connection from './connection';
+import Connection from "./connection";
 
-
-
-
-const host = "192.168.0.106";
-
-
+const host = "192.168.0.101";
 
 /**
  * 默认保持心跳，更新状态
- * 
- * conn对象 
+ *
+ * conn对象
  * 订阅者模式： 监听指定事件，执行任务
  * router模式： 回复给指定的终端
- * 
+ *
  */
 
-
 function main() {
-
   const conn = new Connection({
-    host: host
+    host: host,
   });
-  conn.on("event-name", (command) => {
-
-
-
-  });
-
+  conn.on("event-name", (command) => {});
 }
 main();
-
-
-
-
-
