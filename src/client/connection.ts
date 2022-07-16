a172cbd373abb52e986be5ca0af64f3e8fdda695
yunfeifan <yunfan777@gmail.com>
2022-07-16 09:49:08 +0800

    update nodemon.json

--- src/client/connection.ts
+++ src/client/connection.ts
@@ -1,15 +1,16 @@
-import * as zeromq from 'zeromq';
-import { Ports, createAddress } from '../utils';
-
-
-
+import * as zeromq from "zeromq";
+import { Ports, createAddress } from "../utils";
 
 // (((\d{1,2})|(1\d{2})|(2[0-4]\d)|(25[0-5]))\.){3}((\d{1,2})|(1\d{2})|(2[0-4]\d)|(25[0-5]))
 interface ConnectionProps {
-  host: string,
+  host: string;
 }
 
-const heartbeatInterval = 5000;
+let heartbeatInterval = 15000;
+
+if (process.env.NODE_ENV && process.env.NODE_ENV === "development") {
+  heartbeatInterval = 5000;
+}
 
 /**
  * conn对象，保持连接，更新状态
@@ -21,7 +22,7 @@ export default class Connection {
   constructor(props: ConnectionProps) {
     const self = this;
     this.host = props.host;
-    this.initHeartbeat().then(sock => {
+    this.initHeartbeat().then((sock) => {
       self.heartbeat = sock;
     });
   }
@@ -29,14 +30,14 @@ export default class Connection {
   async initHeartbeat() {
     const protocol = "tcp";
     const address = createAddress(protocol, this.host, Ports["heartbeat"]);
-    const sock = new zeromq.Request;
+    const sock = new zeromq.Request();
     await sock.connect(address);
     console.log(`heartbeat connect ${address}`);
     async function sendBeat() {
       await sock.send("some work");
       const [msg] = await sock.receive();
       console.log("server: %s", msg.toString());
-  
+
       setTimeout(() => {
         sendBeat();
       }, heartbeatInterval);
@@ -45,15 +46,9 @@ export default class Connection {
     return sock;
   }
 
-  
-
-
   send() {
     return {};
   }
 
-  on(eventName: string, callback: (command: string) => void) {
-    
-  }
-
+  on(eventName: string, callback: (command: string) => void) {}
 }
